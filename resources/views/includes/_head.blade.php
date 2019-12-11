fa7cc04f754af833412b393d283035947fc40ae0
Nate Scott <scott.w.nate@gmail.com>
2019-12-11 15:55:22 -0800

    added nav

--- resources/views/includes/_head.blade.php
+++ resources/views/includes/_head.blade.php
@@ -1,3 +1,19 @@
 <header>
-	<img src="img/logos/casa_almendra_logo-02.svg" alt="Casa Almendra Logo" class="logo" title="Casa Almendra Logo">
+    <a href="{{-- {{ route('home')}} --}} " class="logo o-link" id="brand">
+      <img src="img/logos/casa_almendra_logo-02.svg" alt="Paris Valley Ranch Logo" title="Paris Valley Ranch Logo">
+    </a>
+    <nav class="c-top-nav o-container t-slideH">
+        <a href="#" class="c-mobile-navlist-trigger nav-right o-link">&#9776;</a>
+        <ul class="c-navlist">
+            <li class="c-navlist__item">
+                <a href="{{-- {{ route('about')}} --}} " class="c-navlist__link o-link">About</a>
+            </li>
+            <li class="c-navlist__item">
+                <a href="{{-- {{ route('info')}} --}} " class="c-navlist__link o-link">Igunana</a>
+            </li>
+            <li class="c-navlist__item">
+                <a href="{{-- {{ route('contact')}} --}} " class="c-navlist__link o-link">Contact</a>
+            </li>
+        </ul>
+    </nav>
 </header>
