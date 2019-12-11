b8600776cbeb1516f5e0721ded492d2c23ca202a
Nate Scott <scott.w.nate@gmail.com>
2019-12-11 09:40:52 -0800

    fixed header

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,6 +4,9 @@
 	<article class="intro-text">
 		<h1>Amazing Views</h1>
 		<h2>World Class Breaks</h2>
+		<button class="book button">
+		<a href="https://www.vrbo.com/1603268" target="_blank">Book your trip</a>
+		</button>
 	</article>
 </section>
 <section id="main-house">
@@ -74,6 +77,8 @@
 			</aside>
 		</div>
 	</section>
+	<section class="surf-para">
+	</section>
 </section>
 <section id="town">
 	<section class="grid-container">
@@ -109,7 +114,6 @@
 		</button>
 	</article>
 </section>
-
 <section class="map" style="line-height: 0;">
 	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15644.073209261609!2d-86.05177559684437!3d11.406216675026245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f7433d58d72aa43%3A0x4c2581b754de92fb!2sPlaya%20Colorado%2C%20Nicaragua!5e0!3m2!1sen!2sus!4v1574358254725!5m2!1sen!2sus" width="100%" height="600" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 </section>
