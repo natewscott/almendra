543ac998f7a3c3f55932a0f8e89be7265532d42d
Nate Scott <scott.w.nate@gmail.com>
2019-12-16 15:31:13 -0800

    fixing styles up

--- resources/views/includes/_head.blade.php
+++ resources/views/includes/_head.blade.php
@@ -1,6 +1,6 @@
 {{-- <header>
     <a href="{{ route('home')}} " class="logo o-link" id="brand">
-      <img src="img/logos/casa_almendra_logo-02.svg" alt="Paris Valley Ranch Logo" title="Paris Valley Ranch Logo">
+        <img src="img/logos/casa_almendra_logo-02.svg" alt="Paris Valley Ranch Logo" title="Paris Valley Ranch Logo">
     </a>
     <nav class="c-top-nav o-container t-slideH">
         <a href="#" class="c-mobile-navlist-trigger nav-right o-link">&#9776;</a>
@@ -19,38 +19,42 @@
 </header> --}}
 <div id="left-flyout-nav" class="layout-left-flyout visible-sm"></div>
 <div class="layout-right-content">
-  <header class="the-header">
-    <div class="navbar container">
-      
-      <!-- Trigger -->
-      <a class="btn-navbar btn-navbar-navtoggle btn-flyout-trigger" href="#">
-        <span class="icon-bar btn-flyout-trigger"></span>
-        <span class="icon-bar btn-flyout-trigger"></span>
-        <span class="icon-bar btn-flyout-trigger"></span>
-      </a>
-      
-      <!-- Structure -->
-      <nav class="the-nav nav-collapse clearfix">
-     {{--    <ul class="nav nav-pill pull-left">
-          <li class="dropdown">
-            <a href="#">Dogs <b class="caret"></b></a>
-            <ul class="subnav">
-              <li><a href="#">Terrier</a></li>
-              <li><a href="#">Labrador</a></li>
-              <li><a href="#">Pitbull</a></li>
-              <li><a href="#">German Shephard</a></li>
-              <li><a href="#">Chihuahua</a></li>
-              <li><a href="#">Beagle</a></li>
+    <header class="the-header">
+        <div class="navbar container">
+            
+            <!-- Trigger -->
+            <a class="btn-navbar btn-navbar-navtoggle btn-flyout-trigger" href="#">
+                <span class="icon-bar btn-flyout-trigger"></span>
+                <span class="icon-bar btn-flyout-trigger"></span>
+                <span class="icon-bar btn-flyout-trigger"></span>
+            </a>
+            
+            <!-- Structure -->
+            <nav class="the-nav nav-collapse clearfix">
+                <ul class="nav nav-pill pull-left">
+                    <li><a href=" {{ route('home')}} ">
+                        <img src="img/logos/casa_almendra_logo-02.svg" alt="Paris Valley Ranch Logo" title="Paris Valley Ranch Logo">
+                    </a>
+                </li>
+                {{-- <li class="dropdown">
+                    <a href="#">Dogs <b class="caret"></b></a>
+                    <ul class="subnav">
+                        <li><a href="#">Terrier</a></li>
+                        <li><a href="#">Labrador</a></li>
+                        <li><a href="#">Pitbull</a></li>
+                        <li><a href="#">German Shephard</a></li>
+                        <li><a href="#">Chihuahua</a></li>
+                        <li><a href="#">Beagle</a></li>
+                    </ul>
+                </li> --}}
+                
             </ul>
-          </li>
-        
-        </ul> --}}
-        <ul class="nav nav-pill pull-right">
-            <li><a href=" {{ route('home')}} ">Home</a></li>
-          <li><a href=" {{ route('about')}} ">About</a></li>
-          <li><a href=" {{ route('activities')}} ">Activities</a></li>
-          <li><a href=" {{ route('contact')}} ">Contact</a></li>
-        </ul>
-      </nav>
+            <ul class="nav nav-pill pull-right">
+                <li><a href=" {{ route('home')}} ">Home</a></li>
+                <li><a href=" {{ route('about')}} ">About</a></li>
+                <li><a href=" {{ route('activities')}} ">Activities</a></li>
+                <li><a href=" {{ route('contact')}} ">Contact</a></li>
+            </ul>
+        </nav>
     </div>
-  </header>
+</header>
