25817a0f62c26a6f166ddfb317110afe1279b1a6
Nate Scott <scott.w.nate@gmail.com>
2019-12-18 12:05:06 -0800

    added tracker

--- resources/views/includes/_header.blade.php
+++ resources/views/includes/_header.blade.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <title>Casa Almendra @yield('title')</title>
-  <meta name="description" content="@yield('description')">
-  <meta name="keywords" content="@yield('keywords')">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <link rel="apple-touch-icon" href="icon.png">
-  <!-- Place favicon.ico in the root directory -->
-
-  <link href="https://fonts.googleapis.com/css?family=Montserrat|Permanent+Marker&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-  
-  <meta name="theme-color" content="#fafafa">
-</head>
+  <head>
+    <meta charset="utf-8">
+    <title>Casa Almendra @yield('title')</title>
+    <meta name="description" content="@yield('description')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Permanent+Marker&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+    
+    <meta name="theme-color" content="#fafafa">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-154785941-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-154785941-1');
+    </script>
+  </head>
