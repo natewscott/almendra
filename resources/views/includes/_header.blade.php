3de44d14422325b175d8c2598c85ca6a306d6c10
Nate Scott <scott.w.nate@gmail.com>
2019-12-19 14:38:10 -0800

    fixed favico

--- resources/views/includes/_header.blade.php
+++ resources/views/includes/_header.blade.php
@@ -8,11 +8,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="icon.png">
         <!-- Place favicon.ico in the root directory -->
-        <link rel="apple-touch-icon" sizes="180x180" href="{{asset ('/apple-touch-icon.png')}}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{asset ('/favicon-32x32.png')}}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{asset ('/favicon-16x16.png')}}">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="{{asset ('/safari-pinned-tab.svg') }}" color="#5bbad5">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favico/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favico/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favico/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{asset('favico/site.webmanifest') }}">
+        <link rel="mask-icon" href="{{asset('favico/safari-pinned-tab.svg') }}" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#2b5797">
         <meta name="theme-color" content="#ffffff">
         <link href="https://fonts.googleapis.com/css?family=Montserrat|Permanent+Marker&display=swap" rel="stylesheet">
